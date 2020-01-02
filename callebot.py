476bd2764289e337c1212105ee1732b68ba78ae6
Juniorrantila <parti.ivarsson@gmail.com>
2020-01-02 13:00:21 +0100

    Big changes, lots better

--- callebot.py
+++ callebot.py
@@ -1,151 +1,74 @@
-﻿import discord
+import discord
 import random
 import string
 from time import sleep
 from discord.ext import commands
 import asyncio
 import tracemalloc
-
-botid = ''
-
-junior = 'Salanty#4884'
-samuel = 'sampaixd#8896'
-adams = 'Ugn Värmsson#7650'
-adamw = 'Eyy B0ss#9506'
-calle = 'Calle#4643'
-solaf = 'Solaf#7852'
-
-bot = commands.Bot(command_prefix='.')
-
-#async def istyping():
-#	async with message.channel.typing():
-#		sleep(random.randint(1, 5))
-		
-#async def say():
-#	await message.channel.send(mymessage)
-	
-#istypingsay():
-#	async with message.channel.typing():
-#		sleep(random.randint(1, 5))
-#		await message.channel.send(mysessage)
+import other.info as info
 
 
+botid = info.getBotId()
+botid = botid[0]
+
+ingenjörCount = 0
+
+firstname   = info.setUserFirst()
+lastname    = info.setUserLast()
+discordTag  = info.setUserDiscord()
 
 class MyClient(discord.Client):
-	
-	i=0
-	
-	async def on_ready(self):
-		print('Logged on as', self.user)
+    
+    async def on_ready(self):
+        print(f"\nLogged on as {self.user}\n")
 
-	async def on_message(self, message):
-		if message.author == self.user:
-			return
-		
-		if message.content == ('Calle, hjälp!'):
-			await message.channel.send('+ingenjör')
-			await message.channel.send('Calle, ingenjör count')
-			await message.channel.send('Calle, vem bestämmer?')
-			await message.channel.send('Trim!')
-			await message.channel.send('Calle, vad gör en mus bra?')
-			await message.channel.send('Calle, vad är vi?')
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        async def sendMessage(messagelist, istyping=True):
+            print(f"{message.author}:\t{message.content}")
+            for messages in messagelist:
+                if istyping == True:
+                    async with message.channel.typing():
+                        sleep(random.randint(1,5))
+                await message.channel.send(messages)
+                print(f"{self.user}:\t{messages}")
+    
+        def getUser(Userid):
+            return 
+    
+        if message.content == ('Calle, hjälp!'):
+            messagelist = [
+            'ingenjör',
+            'Calle, ingenjör count',
+            'Calle, vem bestämmer?',
+            'Trim!',
+            'Calle, vad gör en mus bra?',
+            'Calle, vad är vi?']
+            await sendMessage(messagelist, istyping=False)
 
-		if message.content == ('Calle, vad är vi?'):
-			for i in range(3):
-				
-				if(i==1):
-					async with message.channel.typing():
-						sleep(random.randint(2, 5))
-					await message.channel.send('Vi är ingenjörer.')
-					sleep(random.randint(1,3))
-				if(i==2):
-					async with message.channel.typing():
-						sleep(random.randint(3, 10))
-					await message.channel.send('OCH INGENJÖRER LÖSER PROBLEM!')
-				
-				
-		if message.content == ('Calle, vad gör en mus bra?'):
-			for i in range(4):
-				
-				if(i==1):
-					async with message.channel.typing():
-						sleep(random.randint(2,5))
-					await message.channel.send('Den där, runda...')
-					sleep(random.randint(1,3))
-				if(i==2):
-					async with message.channel.typing():
-						sleep(random.randint(2,5))
-					await message.channel.send('knappen.')
-					sleep(random.randint(1,3))
-				if(i==3):
-					async with message.channel.typing():
-						sleep(random.randint(2,5))
-					await message.channel.send('...den ska ha en såndär rund knapp i mitten. :)')
-	
-		if message.content == ('this is a test'):
-			mymessage = "404 text not found"
-			async with message.channel.typing():
-				sleep(random.randint(1, 5))
-			await message.channel.send(mymessage)
-			
-		if message.content == ('this is a test too'):
-			mymessage = "505 text was found"
-			istyping()
-			say()
-			
-		if message.content == ('this is a test three'):
-			istypingsay()
-
-		if str(message.author) == adamw: 
-			if message.content == ('Vad fick jag för betyg?'):
-				mymessage = "Du får F!"
-				async with message.channel.typing():
-					sleep(random.randint(1, 5))
-				await message.channel.send(mymessage)
-				sleep(1)
-				async with message.channel.typing():
-					sleep(random.randint(1,5))
-				mymessage = "Sorry, fel Adam."
-				await message.channel.send(mymessage)
-				mymessage = "Du får F!"
-				async with message.channel.typing():
-					sleep(random.randint(1, 5))
-				await message.channel.send(mymessage)
-				
-		if str(message.author) != solaf:
-			if message.content == ('Trim!'):
-				async with message.channel.typing():
-					sleep(random.randint(1,2))
-				await message.channel.send('Ja, trim!')
-				
-		if message.content == ('+ingenjör'):
-			addi(self)
-			mymessage = "Ingenjör count: "+str(self.i)
-			async with message.channel.typing():
-				sleep(random.randint(1, 3))
-			await message.channel.send(mymessage)
-
-		if message.content == ('Calle, ingenjör count'):
-			mymessage = "Ingenjör count: "+str(self.i)
-			async with message.channel.typing():
-				sleep(random.randint(1, 3))
-			await message.channel.send(mymessage)	
-
-#		if message.content == ('Calle, vem bestämmer?'):
-#			mymessage = str('Inte ')  +str(message.author) +str(' iallafall.')
-#			async with message.channel.typing():
-#				sleep(random.randint(1, 5))
-#			await message.channel.send(mymessage)
-
-def addi(self):
-	self.i = self.i + 1
-
-async def istyping():
-	async with message.channel.typing():
-		sleep(random.randint(1, 5))
-	
-async def say():
-	await message.channel.send(mymessage)
-			
+        if message.content == ('Calle, vad är vi?'):
+            messagelist = ['Vi är ingenjörer.', 'OCH INGENJÖRER LÖSER PROBLEM!']    
+            await sendMessage(messagelist)
+        
+        if message.content == ('Calle, vad gör en mus bra?'):
+            messagelist = ['Den där, runda...', 'knappen.', '...den ska ha en såndär rund knapp i mitten.',':)']    
+            await sendMessage(messagelist)
+            
+            
+        if message.author != discordTag[5] and message.content == ('Trim!'):
+            messagelist = ['Ja, trim!']    
+            await sendMessage(messagelist)
+        
+        if message.content == ('Calle, ingenjör count'):
+            messagelist = ['Jag har sagt ingenjörer {ingenjörCount} gånger', 'OCH INGENJÖRER LÖSER PROBLEM!']    
+            await sendMessages(messagelist)
+        
+        if message.content == ('Calle, vem bestämmer?'):
+            messagelist = [f'Inte {message.author} iallafall']    
+            await sendMessage(messagelist)
+        
+        
 client = MyClient()
 client.run(botid)
